6ec033cfd83406fc560708d4ffb1529bed4414ef
jes-sch <jesica_schaffer@hotmail.com>
2024-04-14 19:24:15 -0300

    se elimina la llamada al componente CartWidget del archivo App.jsx y la línea de importación

--- pre-entrega-1/src/App.jsx
+++ pre-entrega-1/src/App.jsx
@@ -1,5 +1,4 @@
 import NavBar from "./components/NavBar"
-import CartWidget from "./components/CartWidget"
 import ItemListContainer from "./components/ItemListContainer";
 import Footer from "./components/Footer";
 
@@ -7,7 +6,6 @@ function App() {
   return (
     <>
     <NavBar />
-    <CartWidget />
     <ItemListContainer mensaje={"♡ El color que tus días necesitan ♡"} />
     <Footer />
     </>
