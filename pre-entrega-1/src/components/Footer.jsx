79c2dd8bfffd95aa011820e00e0f9427b680512a
jes-sch <jesica_schaffer@hotmail.com>
2024-04-27 17:41:05 -0300

    Agrego redes de contacto al componente footer y modifico algunos estilos

--- pre-entrega-1/src/components/Footer.jsx
+++ pre-entrega-1/src/components/Footer.jsx
@@ -2,8 +2,31 @@ const Footer = () => {
     return (
         <div className="container-fluid">
             <div className="row mt-5 p-3">
-                <div className="col textoFooter text-end">
-                    <p>© Copyright Jesica Schäffer 2024 ● Todos los derechos reservados | <b>Botón de arrepentimiento</b></p>
+                <div className="col text-center">
+                    <h4 style={{color:"DarkViolet "}}>Si te gusta mi trabajo, no dudes en contactarme 💌🌸</h4><br></br>
+                    <div className="redes">
+            <a href="https://www.instagram.com/laraschafferart/" target="_blank">
+                <div className="footer__iconos">
+                    <i className="fa-brands fa-instagram"></i>
+                    <p>INSTAGRAM</p>
+                </div>
+            </a>
+            <a href="https://www.behance.net/laraschfferart" target="_blank">
+                <div className="footer__iconos">
+                    <i className="fa-brands fa-behance"></i>
+                    <p>BEHANCE</p>
+                </div>
+            </a>
+            <a href="https://www.linkedin.com/in/lara-sch%C3%A4ffer-95891417a/">
+                <div className="footer__iconos">
+                    <i className="fa-brands fa-linkedin-in"></i>
+                    <p>LINKEDIN</p>
+                </div>
+            </a>
+        </div><br></br>
+                    <div className="col textoFooter text-end">
+                    <p>© Copyright <b>Jesica Schäffer 2024</b> ● Todos los derechos reservados</p>
+                    </div>
                 </div>
             </div>
         </div>
