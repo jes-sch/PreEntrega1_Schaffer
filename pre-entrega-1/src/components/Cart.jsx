25401039e05fffdb3f3f9847a88c7a38648dc667
jes-sch <jesica_schaffer@hotmail.com>
2024-05-26 00:31:08 -0300

    optimizacion y validaciones en mi proyecto

--- pre-entrega-1/src/components/Cart.jsx
+++ pre-entrega-1/src/components/Cart.jsx
@@ -29,19 +29,20 @@ const Cart = () => {
                         <table className="table">
                             <tbody>
                                 <tr>
-                                <td colSpan={5} className="text-end"><button className="btn btn-info text-white fw-bold" onClick={clear}>Vaciar carrito</button></td> 
+                                <td colSpan={6} className="text-end"><button className="btn btn-info text-white fw-bold" onClick={clear}>Vaciar carrito</button></td> 
                                 </tr>
                                 {cart.map(item => (
                                 <tr key={item.id}>
                                     <td className="fw-bold" style={{color:"DarkViolet"}}><img src={item.imagen} alt={item.nombre} width={122} /></td>
                                     <td className="align-middle text-center fw-bold" style={{color:"DarkViolet"}}>{item.nombre}</td>
-                                    <td className="align-middle text-center" style={{color:"DarkViolet"}}>$ {item.precio}</td>
-                                    <td className="align-middle text-center" style={{color:"DarkViolet"}}>{item.quantity}</td>
+                                    <td className="align-middle text-center fw-bold" style={{color:"DarkViolet"}}>$ {item.precio}</td>
+                                    <td className="align-middle text-center fw-bold" style={{color:"DarkViolet"}}>{item.quantity}</td>
+                                    <td className="align-middle text-center fw-bold" style={{color:"DarkViolet"}}>$ {item.quantity * item.precio}</td>
                                     <td className="align-middle text-end"><img src={trash} width={24} alt="Eliminar producto" title="Eliminar producto" onClick={() => {removeItem(item.id)}} /></td>
                                 </tr>
                                 ))}
                                 <tr className="align-middle">
-                                    <td style={{color:"Orchid"}} colSpan={2}><b>TOTAL</b></td>
+                                    <td style={{color:"Orchid"}} colSpan={4}><b>💰 T O T A L 💰</b></td>
                                     <td className="text-center" style={{color:"Orchid"}}><b>$ {getSumProducts()}</b></td>
                                     <td>&nbsp;</td>
                                     <td className="text-end"><Link to={"/checkout"} className="btn btn-info text-white fw-bold">Comprar ✨💰</Link></td>
@@ -52,6 +53,6 @@ const Cart = () => {
                 </div>
             </div>
         )
-    }
+}
 
 export default Cart;
--- pre-entrega-1/src/components/Checkout.jsx
+++ pre-entrega-1/src/components/Checkout.jsx
@@ -1,40 +1,42 @@
-import { useContext, useEffect, useState } from "react";
+import { useContext, useState } from "react";
 import { CartContext } from "./context/CartContext";
-import { addDoc, collection, getDocs, getFirestore} from "firebase/firestore";
+import { addDoc, collection, getFirestore} from "firebase/firestore";
 import { Link } from "react-router-dom";
 
 const Checkout = () => {
-    const {cart, getCountProducts, getSumProducts} = useContext(CartContext);
+    const {cart, clear, getCountProducts, getSumProducts} = useContext(CartContext);
     const [nombre, setNombre] = useState("");
     const [email, setEmail] = useState("");
-    const [telephone, setTelephone] = useState("");
+    const [telefono, settelefono] = useState("");
     const [orderId, setOrderId] = useState("");
 
     const generarOrden = () => {
-        const buyer = {nombre:nombre, email:email, telephone:telephone};
+        if (nombre == "") {
+            return false;
+        }else if (email == "") {
+            return false;
+        } else if (telefono == "") {
+            return false;
+        }
+
+        const buyer = {nombre:nombre, email:email, telefono:telefono};
         const items = cart.map(item => ({id:item.id, title:item.nombre, price:item.precio}));
         const date = new Date();
         const currentDate = `${date.getDate()}-${date.getMonth()+1}-${date.getFullYear()} ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
         const order = {buyer:buyer, items:items, date:currentDate, total:getSumProducts()};
 
-        // Para agregar un nuevo Documento a Firestore:
         const db = getFirestore();
         const ordersCollection = collection(db, "orders");
             addDoc(ordersCollection, order).then(data => {
                 setOrderId(data.id);
+                setNombre("");
+                setEmail("");
+                settelefono("");
+                clear();
             });
-
-    // Para agregar todos los productos mi array JSON a mi BD Firestore
-    /* const db = getFirestore();
-    const itemsCollection = collection(db, "items");
-    arrayProductos.forEach(item => {
-        addDoc(itemsCollection, item);
-    })
-    console.log("PRUEBA SUPERADA : PRODUCTOS COMPLETAMENTE CARGADOS"); */
-
     }
 
-    if (getCountProducts() == 0) {
+    if (getCountProducts() == 0 && !orderId) {
         return (
             <div className="container my-5">
                 <div className="row">
@@ -49,6 +51,7 @@ const Checkout = () => {
     
     return (
         <div className="container">
+            {!orderId ?
             <div className="row">
                 <div className="col">
                     <form>
@@ -62,7 +65,7 @@ const Checkout = () => {
                         </div>
                         <div className="mb-3">
                             <label className="form-label fs-5" style={{color:"LightSeaGreen"}}>Teléfono: </label>
-                            <input type="text" className="form-control" onInput={(e) => {setTelephone(e.target.value)}} />
+                            <input type="text" className="form-control" onInput={(e) => {settelefono(e.target.value)}} />
                         </div>
                             <button type="button" className="btn btn-info text-white fw-bold" onClick={generarOrden}>GENERAR ORDEN 🧷</button>
                     </form>
@@ -84,7 +87,7 @@ const Checkout = () => {
                         </tbody>
                     </table>    
                 </div>
-            </div>
+            </div> : "" }
             <div className="row my-4">
                 <div className="col text-center">
                 {orderId ? <div className="alert alert-info" role="alert">✍🏽 Se generó el siguiente ID a tu compra: <b>{orderId}</b></div> : ""}
